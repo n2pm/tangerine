8944811c27a1312c9f6ea433c856ca28fa44a404
NotNite <hi@notnite.com>
2023-06-27 20:11:20 -0400

    oops

--- src/main/kotlin/pm/n2/tangerine/config/EnumConfigOption.kt
+++ src/main/kotlin/pm/n2/tangerine/config/EnumConfigOption.kt
@@ -3,14 +3,13 @@ package pm.n2.tangerine.config
 import com.google.gson.JsonElement
 import com.google.gson.JsonPrimitive
 
-@Suppress("UNCHECKED_CAST")
 class EnumConfigOption<T : Enum<*>>(
     override val group: String,
     override val name: String,
     override var value: T
 ) : ConfigOption<T>(group, name, value) {
     override fun parse(json: JsonElement) {
-        value = json.asNumber as T
+        value = value.javaClass.enumConstants[json.asInt]
     }
 
     override fun write(): JsonElement = JsonPrimitive(value.ordinal)
