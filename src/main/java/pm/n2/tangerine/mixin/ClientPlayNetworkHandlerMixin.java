a7e714bd670a4a8af45ca39661a40f676688d806
Ali Furkan Yıldız <alifurkanyildiz@gmail.com>
2023-07-23 00:59:02 +0300

    Add ignore rotation module

--- src/main/java/pm/n2/tangerine/mixin/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/pm/n2/tangerine/mixin/ClientPlayNetworkHandlerMixin.java
@@ -2,12 +2,15 @@ package pm.n2.tangerine.mixin;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
+import net.minecraft.network.packet.s2c.play.PlayerPositionLookS2CPacket;
 import net.minecraft.network.packet.s2c.play.UnloadChunkS2CPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import pm.n2.tangerine.modules.movement.IgnoreRotationModule;
 import pm.n2.tangerine.modules.visuals.BlockESPModule;
 
 @Mixin(ClientPlayNetworkHandler.class)
@@ -24,5 +27,17 @@ public class ClientPlayNetworkHandlerMixin {
     @Inject(at = @At("HEAD"), method = "onUnloadChunk")
     private void onChunkUnload(UnloadChunkS2CPacket packet, CallbackInfo ci) {
         BlockESPModule.INSTANCE.unloadChunk(packet.getX() * 16, packet.getZ() * 16);
+    }
+
+    @Inject(method = "onPlayerPositionLook", at = @At("HEAD"))
+    private void onPlayerPositionLook(PlayerPositionLookS2CPacket packet, CallbackInfo ci) {
+        if (IgnoreRotationModule.INSTANCE.getEnabled().getValue()) {
+            var instance = MinecraftClient.getInstance();
+            var player = instance.player;
+            if (player == null) return;
+            var packetAccessor = (PlayerPositionLookS2CPacketAccessor) packet;
+            packetAccessor.setYaw(player.getYaw());
+            packetAccessor.setPitch(player.getPitch());
+        }
     }
 }
---
+++ src/main/kotlin/pm/n2/tangerine/modules/movement/IgnoreRotationModule.kt
@@ -0,0 +1,6 @@
+package pm.n2.tangerine.modules.movement
+
+import pm.n2.tangerine.modules.Module
+import pm.n2.tangerine.modules.ModuleCategory
+
+object IgnoreRotationModule : Module("ignore_rotation", ModuleCategory.MOVEMENT)
