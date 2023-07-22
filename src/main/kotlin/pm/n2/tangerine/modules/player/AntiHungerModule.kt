f294c008f59847a106b4b67f39030575da4e25b4
Ali Furkan Yıldız <alifurkanyildiz@gmail.com>
2023-07-22 23:47:57 +0300

    Improve Anti hunger module

--- src/main/kotlin/pm/n2/tangerine/modules/player/AntiHungerModule.kt
+++ src/main/kotlin/pm/n2/tangerine/modules/player/AntiHungerModule.kt
@@ -1,6 +1,16 @@
 package pm.n2.tangerine.modules.player
 
+import pm.n2.tangerine.Tangerine
+import pm.n2.tangerine.config.BooleanConfigOption
 import pm.n2.tangerine.modules.Module
 import pm.n2.tangerine.modules.ModuleCategory
+import pm.n2.tangerine.modules.movement.FlightModule
 
-object AntiHungerModule : Module("anti_hunger", ModuleCategory.PLAYER)
+object AntiHungerModule : Module("anti_hunger", ModuleCategory.PLAYER) {
+    val turnedOn = BooleanConfigOption(id, "turned_on", false)
+    override val configOptions = listOf(turnedOn)
+
+    override fun onEnabled() {
+        if (turnedOn.value) Tangerine.mc.networkHandler?.sendChatMessage("Turned on")
+    }
+}
